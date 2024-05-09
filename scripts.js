6b7d61b9cb6a6e5bb398f6e5a7c3703f2e56f6c6
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-09 10:33:34 +0200

    Checking if inputs are empty.

--- scripts.js
+++ scripts.js
@@ -5,5 +5,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  if (dividend === '' || divider === '') { // checking if inputs are empty.
+  result.innerText = "Division not performed. Both values are required in inputs. Please Try Again";
+} else {
   result.innerText = Math.floor(dividend / divider); // Added Math.floor to round down the result to the nearest whole number.
+}
 });
