5e85a99b085f09f639e5cdd31635ce17b2d2aa0e
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-09 10:50:50 +0200

    Handle the case where the divider is zero

--- scripts.js
+++ scripts.js
@@ -5,7 +5,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  if (dividend === '' || divider === '') { // checking if inputs are empty.
+
+  if (divider === 0) {
+    result.innerText = "Division not performed. Invalid divider number provided. Please Try Again";
+    console.error("Error: Division by zero");
+  } else if (dividend === '' || divider === '') { // checking if inputs are empty.
   result.innerText = "Division not performed. Both values are required in inputs. Please Try Again";
 } else {
   result.innerText = Math.floor(dividend / divider); // Added Math.floor to round down the result to the nearest whole number.
