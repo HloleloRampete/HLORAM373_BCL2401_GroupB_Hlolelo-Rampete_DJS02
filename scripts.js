63e2a847f887ca84404b6a1e5911fdb1263be403
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-09 11:35:10 +0200

    Using parseInt to check if divider equal 0

--- scripts.js
+++ scripts.js
@@ -6,13 +6,19 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (isNaN(dividend) || isNaN(divider)) {
+  if (isNaN(dividend) || isNaN(divider)) { // checking if inputs are numbers.
+    result.classList.add("critical-error") // Changes HTML body to red.
     result.innerText = "Something critical went wrong. Please reload the page";
     console.error("Error: Non-numeric input provided");
-  } else if (divider === 0) { // checiking if divider equal 0.
+
+
+  } else if (parseInt(divider) === 0) { // checiking if divider equal 0.
     result.innerText = "Division not performed. Invalid number provided. Try again";
     console.error("Error: Division by zero");
+
+    
   } else if (dividend === '' || divider === '') { // checking if inputs are empty.
+    result.classList.add("error-message") // Changes result color to red.
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
   } else {
     result.innerText = Math.floor(dividend / divider); // Added Math.floor to round down the result to the nearest whole number.
